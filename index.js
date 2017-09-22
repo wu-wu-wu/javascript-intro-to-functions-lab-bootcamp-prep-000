2a7f48d736f17c56e26ce6488fbb643e4c2d0bd3
Suzanne Wu <wu.wu.wu.world@gmail.com>
2017-09-22 17:56:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if string = return
+}
