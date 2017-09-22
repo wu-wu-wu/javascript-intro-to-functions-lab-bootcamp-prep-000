e51e84ccc474854f2a36e37f79cfc8b1594021f7
Suzanne Wu <wu.wu.wu.world@gmail.com>
2017-09-22 18:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase === string) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma."){
+  } else if ("I love you, Grandma." === string){
     return "I love you, too."
   }
 }
